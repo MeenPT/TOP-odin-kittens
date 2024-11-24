cd250c0e88be323d95b0bd4de015cb26101b1721
Pakapol Tanachaisith <meen260600@gmail.com>
2024-11-24 23:00:45 +0700

    Build the kitten create form

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,6 @@
 class Kitten < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 1, maximum: 50 }
+  validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :cuteness, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :softness, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
