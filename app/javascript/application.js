6effd906a3399f72b0bf6298b1ddf8189f784759
Pakapol Tanachaisith <meen260600@gmail.com>
2024-11-25 14:14:15 +0700

    Display flash messages

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,26 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('DOMContentLoaded', function () {
+    const closeButton = document.getElementById('notice-alert-close');
+    const alertDiv = document.getElementById('notice-alert');
+  
+    if (closeButton && alertDiv) {
+      closeButton.addEventListener('click', function () {
+        alertDiv.remove(); // Removes the alert div from the DOM
+      });
+    }
+  });
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    const closeButton = document.getElementById('alert-alert-close');
+    const alertDiv = document.getElementById('alert-alert');
+  
+    if (closeButton && alertDiv) {
+      closeButton.addEventListener('click', function () {
+        alertDiv.remove(); // Removes the alert div from the DOM
+      });
+    }
+  });
+  
